b87c5507482c6148da01af22635cca6fb8310336
VanDerWolf79 <141964376+VanDerWolf79@users.noreply.github.com>
2023-10-02 17:57:19 +1100

    Update Contact.vue
    fixed line 26

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,14 +1,20 @@
 <template>
-  <form @submit.prevent="submitForm">
-    <div class="form-group">
-      <label for="name">Name</label>
-      <input type="text" id="name" v-model="name" required />
-    </div>
-    <!-- Add more form fields -->
-    <button type="submit">Submit</button>
-    <p v-if="isSubmitted">Thank you for your submission!</p>
-    <p v-if="hasError">Please fill in all fields.</p>
-  </form>
+  <div class="container contact-form">
+    <h1>Contact Me</h1>
+    <form @submit.prevent="submitForm">
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" id="name" v-model="name" required />
+      </div>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" id="email" v-model="email" required />
+      </div>
+      <button type="submit">Submit</button>
+      <p v-if="isSubmitted">Thank you for your submission!</p>
+      <p v-if="hasError">Please fill in both name and email.</p>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -16,15 +22,14 @@ export default {
   data() {
     return {
       name: "",
-      // Add more form fields
+      email: "",
       isSubmitted: false,
       hasError: false,
     };
   },
   methods: {
     submitForm() {
-      if (this.name && /* Check other fields */) {
-        // Handle form submission logic
+      if (this.name && this.email) {
         this.isSubmitted = true;
         this.hasError = false;
       } else {
@@ -36,3 +41,5 @@ export default {
 };
 </script>
 
+<style scoped>
+</style>
