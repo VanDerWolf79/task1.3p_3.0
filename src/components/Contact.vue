94ce600e506eb8bf4928d7d2ed0603c46b02c9d8
VanDerWolf79 <141964376+VanDerWolf79@users.noreply.github.com>
2023-10-02 17:08:13 +1100

    Add files via upload

---
+++ src/components/Contact.vue
@@ -0,0 +1,38 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div class="form-group">
+      <label for="name">Name</label>
+      <input type="text" id="name" v-model="name" required />
+    </div>
+    <!-- Add more form fields -->
+    <button type="submit">Submit</button>
+    <p v-if="isSubmitted">Thank you for your submission!</p>
+    <p v-if="hasError">Please fill in all fields.</p>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      // Add more form fields
+      isSubmitted: false,
+      hasError: false,
+    };
+  },
+  methods: {
+    submitForm() {
+      if (this.name && /* Check other fields */) {
+        // Handle form submission logic
+        this.isSubmitted = true;
+        this.hasError = false;
+      } else {
+        this.isSubmitted = false;
+        this.hasError = true;
+      }
+    },
+  },
+};
+</script>
+
---
+++ src/components/Projects.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="project-list">
+    <div class="project-item" v-for="project in projects" :key="project.id">
+      <img :src="project.image" alt="Project" />
+      <h2>{{ project.title }}</h2>
+      <p v-if="project.showDetails">{{ project.description }}</p>
+      <button @click="toggleDetails(project)">Toggle Details</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      projects: [
+        { id: 1, title: "Project 1", description: "Description 1", showDetails: false },
+        { id: 2, title: "Project 2", description: "Description 2", showDetails: false },
+        // Add more project items
+      ],
+    };
+  },
+  methods: {
+    toggleDetails(project) {
+      project.showDetails = !project.showDetails;
+    },
+  },
+};
+</script>
